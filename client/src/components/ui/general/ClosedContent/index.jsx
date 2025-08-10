7de72e7b062f1df2745d12cb2ecd6a7e266d8ad5
Viacheslav <slavachirkov92@gmail.com>
2025-08-10 17:45:56 +0300

    Translation: closed content.

--- client/src/components/ui/general/ClosedContent/index.jsx
+++ client/src/components/ui/general/ClosedContent/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
+import { useTranslation } from 'react-i18next'
+
 import { Icon } from '@/components/ui/general/icon'
 
 import styles from './styles.module.scss'
 
-export const ClosedContent = React.memo(({ width, title = 'Coming soon!' }) => {
+export const ClosedContent = React.memo(({ width, title }) => {
+	const { t } = useTranslation()
+
+	title = t('closed_title.coming')
+
 	return (
 		<i title={title} className={styles.closed}>
 			<Icon width={width} height={width} id='close' />
